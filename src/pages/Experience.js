bbdd2c575b9d5f06c6107890a04ff512c8840a70
Jack B <jackabett@gmail.com>
2023-07-03 14:34:54 -0400

    Experience page fix formatting larger and added work icons and box shadows

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -4,33 +4,47 @@ import { useInView } from 'react-intersection-observer';
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
 import "react-vertical-timeline-component/style.min.css"
 import WorkIcon from "@material-ui/icons/Work"
-import UbIcon from "../assets/UbLogo.png";
-import '../styles/Experience.css'
+import UbIcon from "../assets/UB.png";
+import MTB from "../assets/MTB.svg";
+import MTBBlack from "../assets/MTBBlack.svg";
 
+import '../styles/Experience.css'
 
 import { ECBO, EMH, TDPEMH } from '../components/MuiChip.js'
 
 function Experience() {
-    
     const controls = useAnimation();
-    const [ref, inView] = useInView({ triggerOnce: false });
-  
+    const [ref, inView] = useInView({
+        triggerOnce: false, // Set this to false to animate the timeline elements each time they come into view
+        threshold: 0.5, // Adjust this value as per your requirement
+    });
+
+
+    const controlsTitle = useAnimation();
+const [titleRef, titleInView] = useInView({ triggerOnce: false });
+
+
     useEffect(() => {
-      if (inView) {
-        controls.start('visible');
-      } else {
-        controls.start('hidden');
-      }
+        if (inView) {
+            controls.start("visible");
+        }
     }, [controls, inView]);
-    
+
+
+    useEffect(() => {
+        if (titleInView) {
+          controlsTitle.start('visible');
+        } else {
+          controlsTitle.start('hidden');
+        }
+      }, [controlsTitle, titleInView]);
+
     return (
         <div className='experience-pad'>
-
-
-<motion.div
-            ref={ref}
+            <motion.div
+            ref={titleRef}
             initial="hidden"
-            animate={controls}
+            animate={controlsTitle}
             variants={{
               visible: { opacity: 1, scale: 1 },
               hidden: { opacity: 0, scale: 0.5 }
@@ -40,118 +54,156 @@ function Experience() {
               ease: [0, 0.71, 0.2, 1.01]
             }}
           >
-
-<h2 className="heading heading-sec heading-sec__mb-med">
-          <span className="heading-sec__main">Experience</span>
-          <span className="heading-sec__sub">
-            Glad to meet you! I hope you enjoy your stay and here is some information about me :)
-          </span>
-        </h2>
+            <h2 className="heading heading-sec heading-sec__mb-med">
+                <span className="heading-sec__main">Experience</span>
+                <span className="heading-sec__sub">
+                    Here is my Education and Work experience
+                </span>
+            </h2>
+            </motion.div>
             <div className='experience'>
-            <VerticalTimeline lineColor="#3e497a">
-                <VerticalTimelineElement
-                    position={ 'left' }
-                    className='vertical-timeline-element--education'
-                    date='2015 - 2019'
-                    iconStyle={{
-                        background: "#3e497a",
-                        color: "#fff",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        width: "48px",
-                        height: "48px",
-                        borderRadius: "50%",
-                      }}                    
-                      icon={<img src={UbIcon} alt="University at Buffalo"  className="timeline-icon"/>}
+                <VerticalTimeline lineColor="#3e497a">
+                    <VerticalTimelineElement
+                        position={'left'}
+                        className='vertical-timeline-element--education'
+                        date='2015 - 2019'
+                        iconStyle={{
+                            background: "#3e497a",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            borderRadius: "50%",
+                        }}
+                        icon={<img src={UbIcon} alt="University at Buffalo" className="timeline-icon" />}
+                        animate={controls}
+                        initial="hidden"
+                        ref={ref}
+                        variants={{
+                            visible: { opacity: 1, x: 0 },
+                            hidden: { opacity: 0, x: -100 },
+                        }}
+                        intersectionObserverProps={{}}
                     >
-                    <h3 className='verical-timeline-element-title'>
-                        University at Buffalo<br />
-                    </h3>
-                    <h4 className='vertical-timeline-element-subtitle'>Buffalo, NY</h4>
-                    <p>Bachelors of Science in Computer Science <br></br> Data Intensive Computing Certificate</p>
-                 
-                </VerticalTimelineElement>
-            </VerticalTimeline>
-            <VerticalTimeline lineColor="#3e497a">
-                <VerticalTimelineElement
-                    position={ 'right' }
-                    layout={ '1-column-right' }
-                    className='vertical-timeline-element--work'
-                    date='August 2020 - August 2021'
-                    iconStyle={{ background: "#e9d35b", colo: "#fff" }}
-                    icon={<WorkIcon />}
-                >
-                    <h3 className='verical-timeline-element-title'>
-                        M&T Bank, Enterprise Cloud Business Office
-                    </h3>
-                    <h4 className='vertical-timeline-element-subtitle'>
-                        Buffalo, NY
-                    </h4>
-                    <div className='timeline-content'>
-                    <p>Year 1 - Technology Development Program</p>
-                    <p>Enterprise Cloud Business Office - Bank initiative to migrate to Microsoft Azure Cloud infrastructure. Utilized IaC Azure Resource Manager 
-                        Templates to quicky deploy cloud infrastructre and resources for the bank.</p>
-                    </div>
-                    <div className='chip-div'>
-                        <ECBO />                 
-                    </div>
+                        <h3 className='verical-timeline-element-title'>
+                            University at Buffalo<br />
+                        </h3>
+                        <h4 className='vertical-timeline-element-subtitle'>Buffalo, NY</h4>
+                        <p>Bachelors of Science in Computer Science <br></br> Data Intensive Computing Certificate</p>
 
-                
-                </VerticalTimelineElement>
-            </VerticalTimeline>
-            <VerticalTimeline lineColor="#3e497a">
-                <VerticalTimelineElement
-                    position={ 'left' }
-                    className='vertical-timeline-element--work'
-                    date='August 2021 - August 2022'
-                    iconStyle={{ background: "#e9d35b", colo: "#fff" }}
-                    icon={<WorkIcon />}
-                >
-                    <h3 className='verical-timeline-element-title'>
-                        M&T Bank - Enterprise Messaging Hub
-                    </h3>
-                    <h4 className='vertical-timeline-element-subtitle'>
-                        Buffalo, NY
-                    </h4>
-                    <p>Year 2 - Technology Development Program</p>
-                    <p>Enterprise Messaging Hub - Migration of Corticon Business Rules Engine to opensource Drools microservices.</p>
-                    
-                    <div className='chip-div'>
-                    <TDPEMH />                    
-                    </div>
-
-                </VerticalTimelineElement>
-            </VerticalTimeline>
-            <VerticalTimeline lineColor="#3e497a">
-                <VerticalTimelineElement
-                    position={ 'right' }
-                    className='vertical-timeline-element--work'
-                    date='August 2022 - Present'
-                    iconStyle={{ background: "#e9d35b", colo: "#fff" }}
-                    icon={<WorkIcon />}
-                    
-                >
-                    <h3 className='verical-timeline-element-title'>
-                        M&T Bank - Enterprise Messaging Hub
-                    </h3>
-                    <h4 className='vertical-timeline-element-subtitle'>
-                        Buffalo, NY
-                    </h4>
-                    <p> Migration of Messaging Hub from Tibco to Java Spring Boot microservices. Lead migration efforts setting best practices and standards
-                        for microservices to interact seemlessly.</p>
-                    <div className='chip-div'>
-                        <EMH />
-                    </div>
-                        
-                </VerticalTimelineElement>
-            </VerticalTimeline>
-
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
+                <VerticalTimeline lineColor="#3e497a">
+                    <VerticalTimelineElement
+                        position={'right'}
+                        layout={'1-column-right'}
+                        className='vertical-timeline-element--work-tech'
+                        date='August 2020 - August 2021'
+                        iconStyle={{
+                            background: "#e0e622",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            borderRadius: "50%",
+                        }}
+                        icon={<img src={MTBBlack} alt="MTB" className="timeline-icon" />}
+                        animate={controls}
+                        initial="hidden"
+                        ref={ref}
+                        variants={{
+                            visible: { opacity: 1, x: 0 },
+                            hidden: { opacity: 0, x: -100 },
+                        }}
+                        intersectionObserverProps={{}}
+                    >
+                        <h3 className='verical-timeline-element-title'>
+                            M&T Bank, Enterprise Cloud Business Office
+                        </h3>
+                        <h4 className='vertical-timeline-element-subtitle'>
+                            Buffalo, NY
+                        </h4>
+                        <div className='timeline-content'>
+                            <p>Year 1 - Technology Development Program</p>
+                            <p>Enterprise Cloud Business Office - Bank initiative to migrate to Microsoft Azure Cloud infrastructure. Utilized IaC Azure Resource Manager 
+                                Templates to quickly deploy cloud infrastructure and resources for the bank.</p>
+                        </div>
+                        <div className='chip-div'>
+                            <ECBO />                 
+                        </div>
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
+                <VerticalTimeline lineColor="#3e497a">
+                    <VerticalTimelineElement
+                        position={'left'}
+                        className='vertical-timeline-element--work-tech'
+                        date='August 2021 - August 2022'
+                        iconStyle={{
+                            background: "#e0e622",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            borderRadius: "50%",
+                        }}
+                        icon={<img src={MTBBlack} alt="MTB" className="timeline-icon" />}
+                        animate={controls}
+                        initial="hidden"
+                        ref={ref}
+                        variants={{
+                            visible: { opacity: 1, x: 0 },
+                            hidden: { opacity: 0, x: -100 },
+                        }}
+                        intersectionObserverProps={{}}
+                    >
+                        <h3 className='verical-timeline-element-title'>
+                            M&T Bank - Enterprise Messaging Hub
+                        </h3>
+                        <h4 className='vertical-timeline-element-subtitle'>
+                            Buffalo, NY
+                        </h4>
+                        <p>Year 2 - Technology Development Program</p>
+                        <p>Enterprise Messaging Hub - Migration of Corticon Business Rules Engine to open-source Drools microservices.</p>
+                        <div className='chip-div'>
+                            <TDPEMH />                    
+                        </div>
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
+                <VerticalTimeline lineColor="#3e497a">
+                    <VerticalTimelineElement
+                        position={'right'}
+                        className='vertical-timeline-element--work'
+                        date='August 2022 - Present'
+                        iconStyle={{
+                            background: "#ffffff",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            borderRadius: "50%",
+                        }}
+                        icon={<img src={MTB} alt="MTB" className="timeline-icon" />}
+                        animate={controls}
+                        initial="hidden"
+                        ref={ref}
+                        variants={{
+                            visible: { opacity: 1, x: 0 },
+                            hidden: { opacity: 0, x: -100 },
+                        }}
+                        intersectionObserverProps={{}}
+                    >
+                        <h3 className='verical-timeline-element-title'>
+                            M&T Bank - Enterprise Messaging Hub
+                        </h3>
+                        <h4 className='vertical-timeline-element-subtitle'>
+                            Buffalo, NY
+                        </h4>
+                        <p>Migration of Messaging Hub from Tibco to Java Spring Boot microservices. Lead migration efforts setting best practices and standards
+                            for microservices to interact seamlessly.</p>
+                        <div className='chip-div'>
+                            <EMH />
+                        </div>
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
+            </div>
         </div>
-        </motion.div>
-        </div>
-        
-    )
+    );
 }
 
-export default Experience
+export default Experience;
