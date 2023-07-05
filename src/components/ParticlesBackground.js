665d73c1587e456c339fbdb95a4b30bd644215a3
Jack B <jackabett@gmail.com>
2023-07-04 23:02:23 -0400

    Particles fix

--- src/components/ParticlesBackground.js
+++ src/components/ParticlesBackground.js
@@ -1,15 +1,12 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import Particles from 'react-tsparticles';
 import { loadStarsPreset } from 'tsparticles-preset-stars';
 
 const ParticlesBackground = () => {
-  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
-
   useEffect(() => {
     const handleResize = () => {
       const vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
-      setWindowHeight(window.innerHeight);
     };
 
     handleResize();
