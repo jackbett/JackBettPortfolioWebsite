c1578ad5331630e76394432a25c1d50b2ef707c6
Jack B <jackabett@gmail.com>
2023-07-04 21:36:52 -0400

    Testing support for ios

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
+import React, { useLayoutEffect } from 'react';
 import './App.css';
-import Experience from "./pages/Experience";
+import Experience from './pages/Experience';
 import Navbar from './components/Navbar';
-import About from "./pages/About";
-import './font/rhuma_sinera/Rhuma Sinera Regular.ttf'
-import './font/sharpie/Sharpie-Regular.ttf'
-
-import ParticlesComponent from './components/TsParticles.js'; // Update the import path
-import Intro from './pages/Intro.js'
-import Video from './pages/Video.js'
-import Footer from './components/Footer.js'
+import About from './pages/About';
+import './font/rhuma_sinera/Rhuma Sinera Regular.ttf';
+import './font/sharpie/Sharpie-Regular.ttf';
+import ParticlesComponent from './components/TsParticles.js';
+import Intro from './pages/Intro.js';
+import Video from './pages/Video.js';
+import Footer from './components/Footer.js';
 import { ParallaxProvider } from 'react-scroll-parallax';
 
-
-
 function App() {
+  useLayoutEffect(() => {
+    const setAppHeight = () => {
+      const doc = document.documentElement;
+      doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+    };
 
-  const appHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-}
-window.addEventListener('resize', appHeight)
-appHeight()
+    setAppHeight();
+    window.addEventListener('resize', setAppHeight);
+
+    return () => {
+      window.removeEventListener('resize', setAppHeight);
+    };
+  }, []);
 
   return (
     <ParallaxProvider>
-    <div className="App">
-      <Navbar />
-      <div id = 'splash'>
-        <ParticlesComponent />
+      <div className="App">
+        <Navbar />
+        <div id="splash">
+          <ParticlesComponent />
+        </div>
+        <div id="intro">
+          <Intro />
+        </div>
+        <div id="about">
+          <About />
+        </div>
+        <div id="video">
+          <Video />
+        </div>
+        <div id="experience">
+          <Experience />
+        </div>
+        <Footer />
       </div>
-      <div id = 'intro'>
-        <Intro />      
-      </div>
-      <div id = 'about'>
-      <About />
-      </div>
-      <div id = 'video'>
-      <Video />
-      </div>
-      <div id = 'experience'>
-      <Experience />
-      </div>
-    <Footer />
-    </div>
-    </ ParallaxProvider>
+    </ParallaxProvider>
   );
 }
 
--- src/components/TsParticles.js
+++ src/components/TsParticles.js
@@ -1,10 +1,10 @@
-import React, { useCallback, useLayoutEffect, useState } from "react";
+import React, { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadStarsPreset } from "tsparticles-preset-stars";
 import "../styles/TsParticles.css";
 import TypeEffect from "../components/TypeEffect.js";
 import Astronaut from "../assets/Astronaut.png";
-import { Parallax } from "react-scroll-parallax";
+import {Parallax } from "react-scroll-parallax";
 
 const ParticlesComponent = () => {
   const particlesInit = useCallback(async (engine) => {
@@ -22,47 +22,36 @@ const ParticlesComponent = () => {
     },
   };
 
-  const [appHeight, setAppHeight] = useState(window.innerHeight);
-
-  useLayoutEffect(() => {
-    const handleResize = () => {
-      setAppHeight(window.innerHeight);
-    };
   
-    window.addEventListener("resize", handleResize);
-  
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const particlesStyle = {
-    height: `${appHeight}px`,
-    position: "fixed",
-  };
-
   return (
-    <section id="particles" className="particles-sec-pad">
+      <section id="particles" className="particles-sec-pad">
+        
       <Particles
         id="tsparticles"
         init={particlesInit}
         loaded={particlesLoaded}
         options={options}
-        style={particlesStyle}
+        
       />
 
-      <div className="main-particle-container">
+        <div className="main-particle-container">
         <div className="typeEffect">
           <TypeEffect />
         </div>
 
-        <Parallax translateY={["-100%", "100%"]}>
-          <div className="astroNaut">
-            <img src={Astronaut} alt="Astronaut" />
-          </div>
-        </Parallax>
-      </div>
-    </section>
+       
+       
+        <Parallax translateY={['-100%', '100%']}
+      >
+             <div className="astroNaut">
+              <img src={Astronaut} alt="Astronaut" />
+             </div>
+            
+          </Parallax>
+        </div>
+
+       
+      </section>
   );
 };
 
