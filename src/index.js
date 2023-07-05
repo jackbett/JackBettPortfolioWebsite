cf9125be99b1e6bf462f8c2a576e68f36323c8f6
Jack B <jackabett@gmail.com>
2023-07-04 23:35:17 -0400

    reverting changes

--- src/index.js
+++ src/index.js
@@ -5,15 +5,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, viewport-fit=cover"
-    />
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
