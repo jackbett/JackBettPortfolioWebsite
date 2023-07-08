30491f954983b455c83ce409133ef671503e73f3
Jack B <jackabett@gmail.com>
2023-07-08 11:38:18 -0400

    testing setting height for ios full screen

--- src/components/ParticlesBackground.js
+++ src/components/ParticlesBackground.js
@@ -1,8 +1,29 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Particles from 'react-tsparticles';
 import { loadStarsPreset } from 'tsparticles-preset-stars';
 
 const ParticlesBackground = () => {
+  const [backgroundHeight, setBackgroundHeight] = useState(0);
+
+  useEffect(() => {
+    const calculateBackgroundHeight = () => {
+      const doc = document.documentElement;
+      const windowHeight = window.innerHeight || 0;
+      const searchBarHeight = window.screen.height - windowHeight;
+      const backgroundHeight = windowHeight + searchBarHeight;
+      doc.style.setProperty('--background-height', `${backgroundHeight}px`);
+      setBackgroundHeight(backgroundHeight);
+    };
+
+    calculateBackgroundHeight();
+
+    window.addEventListener('resize', calculateBackgroundHeight);
+
+    return () => {
+      window.removeEventListener('resize', calculateBackgroundHeight);
+    };
+  }, []);
+
   const particlesInit = useCallback(async (engine) => {
     await loadStarsPreset(engine);
   }, []);
@@ -19,19 +40,21 @@ const ParticlesBackground = () => {
   };
 
   const backgroundStyle = {
-    height: `${window.screen.height}px`,
+    height: `${backgroundHeight}px`,
     minHeight: '100vh',
     flex: '1',
   };
 
   return (
-    <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={options}
-      style={backgroundStyle}
-    />
+    <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 0}}>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={options}
+        style={backgroundStyle}
+      />
+    </div>
   );
 };
 
